54d555df8b78196b7e94ea7f055dc2ca227e208e
Euglenach <midorimushiiii@gmail.com>
2024-11-10 00:38:53 +0900

    create project and import package

---
+++ MiniverseClient/Assets/Scripts/MemoryPackMagicOnionSerializerInitializer.cs
@@ -0,0 +1,15 @@
+﻿using MagicOnion.Serialization;
+using MagicOnion.Serialization.MemoryPack;
+using UnityEngine;
+
+namespace Miniverse
+{
+    public class MemoryPackMagicOnionSerializerInitializer
+    {
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        public static void Initialize()
+        {
+            MagicOnionSerializerProvider.Default = MemoryPackMagicOnionSerializerProvider.Instance;
+        }
+    }
+}
