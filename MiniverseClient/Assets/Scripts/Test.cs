c3220e350c4495d071d0a309fd1bde829e6cc219
Euglenach <midorimushiiii@gmail.com>
2024-12-08 05:46:17 +0900

    サービスディスカバリ疎通確認

--- MiniverseClient/Assets/Scripts/Test.cs
+++ MiniverseClient/Assets/Scripts/Test.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Cysharp.Threading.Tasks;
+using Miniverse.WebAPI;
 using MiniverseShared.MessagePackObjects;
+using MiniverseShared.WebAPI;
 using StreamingHubs;
 using UnityEngine;
 
@@ -8,8 +10,18 @@ public class Test : MonoBehaviour
 {
     private async UniTask Start()
     {
+        // CreateRoom().Forget();
+        // CreateRoom().Forget();
+        // CreateRoom().Forget();
+        
+        var response = await MessagePackWebAPI.GetAsync<MagicOnionURLRequest, MagicOnionURLResponse>();
+        Debug.Log(response.URL);
+    }
+
+    async UniTaskVoid CreateRoom()
+    {
         var player = new Player{ Ulid = Ulid.NewUlid(), Name = "Test" };
         var hub = await MatchingHub.CreateAsync(player);
-        await hub.JoinAsync(Ulid.NewUlid());
+        await hub.CreateRoomAsync();
     }
 }
---
+++ MiniverseClient/Assets/Scripts/WebAPI/MessagePackWebAPI.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using Cysharp.Net.Http;
+using Cysharp.Threading.Tasks;
+using MessagePack;
+using MiniverseShared.WebAPI;
+using UnityEngine;
+
+namespace Miniverse.WebAPI
+{
+    public static class MessagePackWebAPI
+    {
+        private static readonly string baseUrl = "http://localhost:5277/api/";
+        private static readonly HttpClient httpClient = new(new HttpClientHandler());
+        
+        public static async UniTask<TResponse> GetAsync<TRequest, TResponse>(CancellationToken cancellationToken = default)
+        {
+            var response = await httpClient.GetAsync(baseUrl + typeof(TRequest).Name, cancellationToken);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            
+            return MessagePackSerializer.Deserialize<TResponse>(responseBytes);
+        }
+        
+        public static async UniTask<TResponse> PostAsync<TResponse>(object request, CancellationToken cancellationToken = default)
+        {
+            var bytes = MessagePackSerializer.Serialize(request);
+            var content = new ByteArrayContent(bytes);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            var response = await httpClient.GetAsync(baseUrl + request.GetType().Name, cancellationToken);
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
+            
+            return MessagePackSerializer.Deserialize<TResponse>(responseBytes);
+        }
+    }
+}
